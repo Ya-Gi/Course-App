79b8c5a737916fb66f5edfda63b3ad3511cfbe36
Attilla <yakyar4@gmail.com>
2023-04-07 00:56:38 +0300

    m1 Redux tool kit ile kurs app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import CourseForm from "./component/CourseForm";
+import CourseList from "./component/CourseList";
+import CourseSearch from "./component/CourseSearch";
+import CourseValue from "./component/CourseValue";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container is-fluid">
+      <CourseForm/>
+      <CourseList/>
+      <CourseSearch/>
+      <CourseValue/>
     </div>
   );
 }
